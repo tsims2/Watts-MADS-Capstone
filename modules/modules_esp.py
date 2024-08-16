605d578a5d16ed811f41cfc60595fb251fb405d4
Tyler Sims <92755645+tsims2@users.noreply.github.com>
2024-08-16 00:01:43 -0400

    Update modules_esp.py

--- modules/modules_esp.py
+++ modules/modules_esp.py
@@ -3,18 +3,6 @@ from .settlement_api_dispatch_data import DispatchDataProcessor
 from .settlement_cs_performance_calculations import CSPerformanceCalculator
 from .settlement_cs_report_generation  import CSReportGenerator
 
-color_palette = [('#FF5400'),  # orange
-                    ('#FFC907'),  # yellow
-                    ('#072B60'),  # royal blue
-                    ('#00C9AZ'),  # green
-                    ('#59C8E3'), # sky blue
-                    ('#020244'), # navy
-                    ('#EBF2F5'), # light blue
-                    ('#363C49'), # gray
-                    ('#0078A6'), #Teal
-                    ('#12B34C'), #Green
-                    ]  
-
 def get_float_value(self, value_str):
     try:
         return float(value_str)
